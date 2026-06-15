8ba582ae97c331d4f5e94df3802a0e86cc10b7a5
Gaspard Ringuenet <gaspard.ringuenet@gmail.com>
2026-06-15 17:08:39 +0200

    better echotype frequency response plots

--- src/escore/echotypes/utils.py
+++ src/escore/echotypes/utils.py
@@ -1,3 +1,5 @@
+import re
+
 import pandas as pd
 import xarray as xr
 from echoregions.regions2d import Regions2D
@@ -19,3 +21,56 @@ def _select_bbox_data(ds: xr.Dataset, var: str, region_row: pd.Series):
         ping_time=slice(region_row["region_bbox_left"], region_row["region_bbox_right"]),
         depth=slice(region_row["region_bbox_top"], region_row["region_bbox_bottom"]),
     )
+
+
+def _format_echotype_dataframe(
+    df: pd.DataFrame,
+    ref_channel_idx: int | None = None,
+) -> pd.DataFrame:
+    """Format echotype dataframe data. Channel columns are renamed
+    (channel_{i}\_Sv -> channel\_{i}). If ref_channel_idx is provided,
+    the channel\_{i} value will contain Sv difference between the original
+    channel_{i}\_Sv and channel_{ref_channel_idx}\_Sv.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Echotype dataframe containing per-channel Sv data in columns named channel_{i}\_Sv.
+    ref_channel_idx : int | None, optional
+        Index of the reference channel to subtract to all channels, by default None
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe with formatted channel columns.
+    """
+
+    # Rename all channels 'channel_{i}_Sv' -> 'channel_{i}' and fetch ids
+    channels_idx: list[int] = []
+
+    def rename_channels(col: str) -> str:
+        m = re.fullmatch(r"channel_(\d+)_Sv", col)
+        if m:
+            i = int(m.group(1))
+            channels_idx.append(i)
+            return f"channel_{i}"
+        return col
+
+    df = df.rename(rename_channels, axis=1)
+
+    if ref_channel_idx is not None:
+        ref_col = f"channel_{int(ref_channel_idx)}"
+
+        # Check that reference channel exists in dataframe
+        if ref_col not in df.columns:
+            raise KeyError(f"Reference channel column {ref_col} not found in dataframe")
+
+        # Fetch reference column data
+        ref_values = df[ref_col].copy()
+
+        # Subtract reference channel from all channels
+        for i in channels_idx:
+            col = f"channel_{i}"
+            df[col] = df[col] - ref_values
+
+    return df
--- src/escore/echotypes/workflow.py
+++ src/escore/echotypes/workflow.py
@@ -1,7 +1,6 @@
 import shutil
 from dataclasses import asdict, dataclass
-from pathlib import Path
-from typing import Literal, Tuple
+from typing import List, Literal, Sequence, Tuple
 
 import holoviews as hv
 import hvplot.pandas  # noqa
@@ -16,7 +15,7 @@ from echoregions.regions2d import Regions2D
 from sklearn.pipeline import Pipeline
 
 from escore.echotypes.paths import ResultsPathManager
-from escore.echotypes.utils import _select_bbox_data, _select_region_row
+from escore.echotypes.utils import _format_echotype_dataframe, _select_bbox_data, _select_region_row
 from escore.utils.plot import plot_channels, plot_rgb
 from escore.utils.sklearn import classes_to_segments, stack_for_sklearn, unstack_sklearn_preds
 
@@ -81,6 +80,7 @@ class EchotypeWorkflow:
     def set_current(
         self,
         region_id: int,
+        verbose: bool = True,
     ) -> None:
         """Change the current region / echotype being processed. Load processing data if
         there is some.
@@ -89,9 +89,14 @@ class EchotypeWorkflow:
         ----------
         region_id : int
             Id of the region in the Regions2D annotation object.
-        """
+        verbose : bool, optional
+            Print processing info, by default True
 
-        # TODO: Update "last" field in state dict.
+        Raises
+        ------
+        ValueError
+            If requested region id does not exist in the region.data DataFrame.
+        """
 
         if region_id not in self.regions.data["region_id"].values:
             raise ValueError(
@@ -100,7 +105,7 @@ class EchotypeWorkflow:
             )
 
         # Display message if a current id already exists
-        if self.current_ is not None:
+        if self.current_ is not None and verbose:
             if self.current_.region_id != region_id:
                 print(
                     f"Switching current id from {self.current_.region_id} to {region_id}. "
@@ -113,18 +118,18 @@ class EchotypeWorkflow:
         self.current_ = CurrentRegion(region_id, self._get_region_data(region_id))
 
         # Load recipe, pipeline, and results (if they exist)
-        self._load_current()
+        self._load_current(verbose)
 
         # Display warning about the status of region
         state_dict = self._load_state()
         status: str = state_dict["status"][region_id]
-        print(f"\nWARNING: Current region has status {status.upper()}.")
+        print(f"WARNING: Current region has status {status.upper()}.") if verbose else None
 
         # Update workflow state
         state_dict["last"] = region_id
         self._dump_state(state_dict)
 
-        print("Ready for processing!")
+        print("Ready for processing!") if verbose else None
 
     def reset_current(self):
         """Resets current echotype state as blank. Only region_id and region_data
@@ -373,16 +378,34 @@ class EchotypeWorkflow:
 
     def get_echotype_dataframe(
         self,
-        use_frequency_var: xr.DataArray | None = None,
+        add_region_cols: List[str] | Literal["default"] = "default",
     ) -> pd.DataFrame:
-        """Return selected echotype as DataFrame if is exists."""
+        """Return selected echotype as DataFrame if is exists
+
+        Parameters
+        ----------
+        add_region_cols : List[str] | Literal[&quot;default&quot;], optional
+            Region data to append to each echotype row. If "default", columns
+            "region_id" and "region_class" are used. To add new columns, specify
+            column names as a list. By default "default".
+
+        Returns
+        -------
+        pd.DataFrame
+            DataFrame containing echotype data with default columns
+            (depth, time, channel_0_Sv, ..., channel_n_Sv, region_id, region_class)
+            and requested additional region columns (if they exist).
+
+        Raises
+        ------
+        ValueError
+            If add_region_cols is neither "default" nor a list of strings.
+        """
 
         da_echotype = self.get_echotype_dataarray()
 
-        if use_frequency_var is not None:
-            da_echotype = da_echotype.assign_coords(channel=use_frequency_var)
-
-        df_echotype = (
+        # Format to DataFrame with columns: depth, time, channel_0_Sv, ..., channel_n_Sv
+        df = (
             da_echotype.to_dataframe(
                 name="Sv",
                 dim_order=["depth", "ping_time", "channel"],
@@ -393,10 +416,85 @@ class EchotypeWorkflow:
                 columns="channel",
                 values="Sv",
             )
-            .reset_index(drop=True)
         )
 
-        return df_echotype
+        # Rename channel columns
+        df = df.rename(
+            columns={col: f"channel_{i}_Sv" for i, col in enumerate(df.columns)}
+        ).reset_index()
+
+        # Add region attributes
+        # Select region row
+        region_row = _select_region_row(self.regions, self.current_.region_id, close=False)  # type: ignore
+
+        # Append default columns
+        default_cols = ["region_id", "region_class"]
+        if isinstance(add_region_cols, list) and len(add_region_cols) > 0:
+            # Keep defaults and add additional
+            cols = default_cols + [col for col in add_region_cols if col not in default_cols]
+        elif add_region_cols == "default":
+            cols = default_cols
+        else:
+            raise ValueError(
+                'add_region_cols argument must be either "default" or list of strings.'
+                f"Got {add_region_cols}."
+            )
+
+        # Add new columns for all samples in echotype
+        for col in cols:
+            # Avoid potential confusion between sample and region attribute (e.g. "depth")
+            colname = col if col not in df.columns else f"region_{col}"
+
+            # Add new column and print warning if column does not exist
+            try:
+                df[colname] = [region_row[col]] * len(df)
+            except KeyError:
+                print(
+                    f"Warning: column {col} does not exist in regions data. "
+                    f"Available columns {list(region_row.index)}."
+                )
+
+        return df
+
+    # Step 5 - Export entire echotypes dataset as .csv file
+    def export_completed(
+        self,
+        add_region_cols: List[str] | Literal["default"] = "default",
+    ) -> pd.DataFrame:
+        """Export all echotypes as a single DataFrame. Loop through
+        "completed" regions and convert to dataframes using the
+        `.get_echotype_dataframe` method. Echotypes are assigned unique
+        int ids.
+
+        Parameters
+        ----------
+        add_region_cols : List[str] | Literal[&quot;default&quot;], optional
+            Regions data to insert. Passed to get_echotype_dataframe, by default "default"
+
+        Returns
+        -------
+        pd.DataFrame
+            DataFrame containing echotype data with default columns
+            (echotype_id, depth, time, channel_0_Sv, ..., channel_n_Sv, region_id, region_class)
+            and requested additional region columns (if they exist).
+        """
+
+        state_dict = self._load_state()
+        region_status_dict = state_dict["status"]
+
+        df_list = []
+
+        for i, (region_id, status) in enumerate(region_status_dict.items()):
+            if status != "completed":
+                continue
+            self.set_current(region_id, verbose=False)
+            df = self.get_echotype_dataframe(add_region_cols)
+            df.insert(0, "echotype_id", i)
+            df_list.append(df)
+
+        df_tot = pd.concat(df_list)
+
+        return df_tot
 
     #### Workflow I/O utilities ####
 
@@ -433,12 +531,17 @@ class EchotypeWorkflow:
         else:
             return self._init_state()
 
-    def _load_current(self) -> None:
+    def _load_current(self, verbose: bool) -> None:
         """Load a region / echotype processing data. Update CurrentRegion object.
         Directory state assumption: existence of a region's result folder is equivalent
         to this folder containing all necessary files (This assumption is enforced by
         dumping all files at once and deleting the directory in case of failure -
         see .dump_current).
+
+        Parameters
+        ----------
+        verbose : bool
+            Print loading info.
 
         Raises
         ------
@@ -452,7 +555,7 @@ class EchotypeWorkflow:
 
         # If the region's results directory exists: we assume it contains data
         if self.paths.region_results(region_id).exists():
-            print("Loading recipe for current region...")
+            print("Loading recipe for current region...") if verbose else None
             # Select paths
             segmenter_path = self.paths.segmenter(region_id)
             recipe_path = self.paths.recipe(region_id)
@@ -465,7 +568,7 @@ class EchotypeWorkflow:
 
         # Else: we assume the directory is empty and there is no data to load
         else:
-            print("No saved recipe to load.")
+            print("No saved recipe to load.") if verbose else None
             pass
 
     #### Data selection utilities ####
@@ -686,62 +789,123 @@ class WorkflowDataVisualizer:
     def echotype_frequency_response(
         self,
         relative_to_channel: int | None = None,
-        **hvplot_kwrgs,
+        channel_values: Sequence[float] | None = None,
+        channel_varname: str = "channel",
     ):
+        # Get echotype data as dataframe
+        df = self.parent.get_echotype_dataframe()
 
-        da_echotype = self.parent.get_echotype_dataarray()
-        freqs = self.parent.data["frequency_nominal"]
+        # Format dataframe (rename channel cols + compute Sv diff if necessary)
+        df = _format_echotype_dataframe(df, relative_to_channel)
 
-        # Replace channel coordinate with frequency_nominal and rename dimension
-        da_echotype = da_echotype.assign_coords(channel=freqs).rename(
-            {"channel": "frequency_nominal"}
+        # Fetch channel columns
+        channel_cols = df.filter(regex=r"^channel_\d+").columns.to_list()
+
+        # Build summary dataframe
+        summary = pd.DataFrame({"mean": df[channel_cols].mean(), "sd": df[channel_cols].std()})
+        summary.index.name = "channel"
+        summary = summary.reset_index()
+
+        # Use custom variable name & values
+        if channel_values is not None:
+            summary[channel_varname] = channel_values
+        else:
+            summary[channel_varname] = summary["channel"]
+
+        # Build hvplot elements
+        curve = summary.hvplot.line(x=channel_varname, y="mean", line_width=2)
+        points = summary.hvplot.scatter(x=channel_varname, y="mean", size=6)
+        errors = summary.hvplot.errorbars(x=channel_varname, y="mean", yerr1="sd")
+
+        # Overlay
+        plot = curve * points * errors
+
+        # Ensure margin around x values
+        try:
+            xmin, xmax = min(channel_values), max(channel_values)  # type: ignore
+            xrange = xmax - xmin
+            plot = plot.opts(xlim=(xmin - 0.05 * xrange, xmax + 0.05 * xrange))
+        except Exception:
+            pass
+
+        return plot
+
+    def echotype_frequency_response_hist(
+        self,
+        relative_to_channel: int | None = None,
+        channel_values: Sequence[float] | None = None,
+        channel_varname: str = "channel",
+        **hvplot_kwrgs,
+    ) -> hv.element.Histogram:
+        """Histogram of the echotype's Sv (or Delta Sv) values, by channel.
+        Delta Sv if computed if 'relative_to_channel' is specified.
+
+        Parameters
+        ----------
+        relative_to_channel : int | None, optional
+            Index of the reference channel to subtract to all channels, by default None
+        channel_values : Sequence[float] | None, optional
+            Channel values to use instead of default 'channel_{i}' labels.
+            Must match the order of channel in the workflow's acoustic dataset.
+            By default None
+        channel_varname : str, optional
+            Name of the channel variable, by default "channel"
+
+        Returns
+        -------
+        hv.element.Histogram
+            Histogram plot using holoviews.
+
+        Raises
+        ------
+        ValueError
+            If channel_values length does not match the number of channel columns.
+        """
+
+        # Get echotype data as dataframe
+        df = self.parent.get_echotype_dataframe()
+
+        # Format dataframe (rename channel cols + compute Sv diff if necessary)
+        df = _format_echotype_dataframe(df, relative_to_channel)
+
+        # Fetch channel columns
+        channel_cols = df.filter(regex=r"^channel_\d+").columns.to_list()
+
+        # Use custom variable name & values
+        if channel_values is not None:
+            if not len(channel_values) == len(channel_cols):
+                raise ValueError(
+                    f"Number of provided channel values ({len(channel_values)})"
+                    f"does not match number of channels: {len(channel_cols)}."
+                )
+            # Rename channel cols to contain channel values
+            df = df.rename(columns={col: val for col, val in zip(channel_cols, channel_values)})
+
+            # Use provided values as new channel columns
+            channel_values_final = list(channel_values)
+        else:
+            # Use default channel columns
+            channel_values_final = channel_cols
+
+        # Drop reference channel (all zeros)
+        if relative_to_channel is not None:
+            ref_channel_value = channel_values_final[relative_to_channel]
+            df = df.drop(ref_channel_value, axis=1)  # Drop
+            # Remove reference channel from channel values
+            channel_values_final.remove(ref_channel_value)  # type: ignore
+
+        # Melt channel columns to long format
+        df = df.melt(
+            id_vars=["depth", "ping_time"],
+            value_vars=channel_values_final,
+            var_name=channel_varname,
+            value_name="value",
         )
 
-        # Absolute frequency response
-        if relative_to_channel is None:
-            ds_afr = xr.Dataset(
-                {
-                    "mean": da_echotype.mean(dim=["depth", "ping_time"]),
-                    "std": da_echotype.std(dim=["depth", "ping_time"]),
-                }
-            )
+        # Build hvplot histogram
+        defaults = {"normed": True, "bins": 40, "alpha": 0.7}
+        hvplot_kwrgs = defaults | hvplot_kwrgs
 
-            hvplot_kwrgs_defaults = {
-                "title": "Absolute Frequency Response",
-                "xlabel": "Channel Frequency [Hz]",
-                "ylabel": "Mean SV [dB]",
-            }
-            hvplot_kwrgs |= hvplot_kwrgs_defaults
+        hist = df.hvplot.hist(y="value", by=channel_varname, **hvplot_kwrgs)
 
-            plot_afr = (
-                ds_afr.hvplot.line(x="frequency_nominal", y="mean")
-                * ds_afr.hvplot.scatter(x="frequency_nominal", y="mean")
-                * ds_afr.hvplot.errorbars(x="frequency_nominal", y="mean", yerr1="std")
-            ).opts(**hvplot_kwrgs)
-
-            return plot_afr
-
-        # Relative frequency response
-        ref = relative_to_channel
-        da_diff = da_echotype - da_echotype.isel(frequency_nominal=ref)
-        ds_rfr = xr.Dataset(
-            {
-                "mean": da_diff.mean(dim=["depth", "ping_time"]),
-                "std": da_diff.std(dim=["depth", "ping_time"]),
-            }
-        )
-
-        hvplot_kwrgs_defaults = {
-            "title": "Relative Frequency Response",
-            "xlabel": "Channel Frequency [Hz]",
-            "ylabel": f"Mean ΔSV (relative to ch. {ref}) [dB]",
-        }
-        hvplot_kwrgs |= hvplot_kwrgs_defaults
-
-        plot_rfr = (
-            ds_rfr.hvplot.line(x="frequency_nominal", y="mean")
-            * ds_rfr.hvplot.scatter(x="frequency_nominal", y="mean")
-            * ds_rfr.hvplot.errorbars(x="frequency_nominal", y="mean", yerr1="std")
-        ).opts(**hvplot_kwrgs)
-
-        return plot_rfr
+        return hist  # type: ignore
